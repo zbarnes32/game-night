d6b2018ba70ae0a734cc16326c5c6d32309462f5
Zach Barnes <zachary.barnes.32@gmail.com>
2024-06-17 16:24:44 -0600

    Button increaseScore logs message

--- app/controllers/GameController.js
+++ app/controllers/GameController.js
@@ -2,4 +2,8 @@ export class GameController {
     constructor() {
         console.log('This loaded from the GameController')
     }
+
+    increaseScore() {
+        console.log('This is where the score will increase')
+    }
 }
