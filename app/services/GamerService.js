19b6cab08694d39e654bf227a2b73de84db78a2a
Zach Barnes <zachary.barnes.32@gmail.com>
2024-06-17 23:42:54 -0600

    Increase gamer score in console.log

--- app/services/GamerService.js
+++ app/services/GamerService.js
@@ -1,6 +1,12 @@
+import { AppState } from "../AppState.js"
+
+
 class GamerService {
     increaseScore() {
         console.log('Increasing the score, but from the service file.')
+        const gamers = AppState.gamers
+        gamers[0].score++
+        console.log('Increased Player Score:', gamers[0].score);
     }
 }
 
